cfe6f332ba08c9d4c08ee24b4194520a76f5c1b4
sronga <sr899@georgetown.edu>
2018-06-06 16:55:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,34 +6,49 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  input = input_to_index(input)
-  token = 'X'
-
-	until valid_move?(board, input)
-    input = gets.strip
-    input = input_to_index(input)
-  end
-
-  move(board, input, token)
-  display_board(board)
-end
-
-def valid_move?(board, input)
-  input.between?(0, 8) && position_taken?(board, input)
-end
-
-def position_taken?(board, index_number)
- board[index_number] == " " or board[index_number] == "" or board[index_number] == nil
-end
-
 def input_to_index(input)
-  input = input.to_i
-  input = input - 1
+  input.to_i - 1
 end
 
-def move(board, user_input, character='X')
-  board[user_input] = character
+def move(array,index,token="X")
+  array[index] = token
 end
+
+def turn(board)
+  index = "INVALID"
+
+    until valid_move?(board, index) == true
+        puts "Please enter 1-9:"
+        input = gets.strip
+
+        index = input_to_index(input)
+    end
+
+  if valid_move?(board, index)
+      move(board, index)
+  end
+display_board(board)
+end
+
+def position_taken?(board, index)
+    if board[index] == " "
+      false
+    elsif board[index] == ""
+      false
+    elsif board[index] == nil
+      false
+    elsif board[index] == "X"
+      true
+    elsif board[index] == "O"
+      true
+    end
+end
+
+def valid_move?(board, index)
+index.is_a?(Integer) && index.between?(0,8) && !position_taken?(board, index)
+end
+
+
+puts "Welcome to Tic Tac Toe!"
+display_board(board)
+#turn(board)
