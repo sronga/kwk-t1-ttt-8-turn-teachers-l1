482adf27d339c98199ec82967aa545c4b4214284
sronga <sr899@georgetown.edu>
2018-06-06 17:01:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,19 +9,16 @@ def display_board(board)
 end
 
 def turn(board)
-  index = "INVALID"
+    puts "Please enter 1-9:"
+    input = gets.strip
 
-    until valid_move?(board, index) == true
-        puts "Please enter 1-9:"
-        input = gets.strip
-
-        index = input_to_index(input)
-    end
+    index = input_to_index(input)
 
   if valid_move?(board, index)
       move(board, index)
+      display_board(board)
+  else turn(board)
   end
-display_board(board)
 end
 
 def input_to_index(input)
