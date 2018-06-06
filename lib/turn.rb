f3fee5c3b8f29a70b20d1fc22c85947e0c5b0959
sronga <sr899@georgetown.edu>
2018-06-06 16:54:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,9 @@
-board = [" "," "," "," "," "," "," "," "," "]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-  input.to_i - 1
-end
-
-def move(array,index,token="X")
-  array[index] = token
 end
 
 def turn(board)
@@ -27,31 +17,23 @@ def turn(board)
     input = input_to_index(input)
   end
 
-  if valid_move?(board, index)
-      move(board, index)
-  end
-display_board(board)
+  move(board, input, token)
+  display_board(board)
 end
 
-def position_taken?(board, index)
-    if board[index] == " "
-      false
-    elsif board[index] == ""
-      false
-    elsif board[index] == nil
-      false
-    elsif board[index] == "X"
-      true
-    elsif board[index] == "O"
-      true
-    end
+def valid_move?(board, input)
+  input.between?(0, 8) && position_taken?(board, input)
 end
 
-def valid_move?(board, index)
-index.is_a?(Integer) && index.between?(0,8) && !position_taken?(board, index)
+def position_taken?(board, index_number)
+ board[index_number] == " " or board[index_number] == "" or board[index_number] == nil
 end
 
+def input_to_index(input)
+  input = input.to_i
+  input = input - 1
+end
 
-puts "Welcome to Tic Tac Toe!"
-display_board(board)
-#turn(board)
+def move(board, user_input, character='X')
+  board[user_input] = character
+end
